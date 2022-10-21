47e3016a0a4076352ff52db534d74e8a8b9f2c7a
GabiProg <gabrielaterese07@gmail.com>
2022-10-21 00:12:53 -0300

    Routes /signin and /signup styled-components completed

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,16 @@ import GlobalStyle from "../assets/GlobalStyle.js";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Sidebar from "./Sidebar";
 import HashtagPage from "./HashtagPage.js";
+import SignIn from "./SignIn.js";
+import SignUp from "./SignUp.js";
 
 export default function App() {
   return (
     <BrowserRouter>
       <GlobalStyle />
       <Routes>
+        <Route path='/signin' element={<SignIn />} />
+        <Route path='/signup' element={<SignUp />} />
         <Route path='/sidebar' element={<Sidebar />} />
         <Route path='/hashtag/:hashtag' element={<HashtagPage />} />
       </Routes>
