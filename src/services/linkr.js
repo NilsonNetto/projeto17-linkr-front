0c2d13e9655b4923502fc66e58a717f58d690f03
Norival <norivaljunior.jf@gmail.com>
2022-10-21 16:44:38 -0300

    1. User page created 2. User search function created, header update

--- src/services/linkr.js
+++ src/services/linkr.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const URL = 'http://localhost:4000';
+const URL = "http://localhost:4000";
 
 function getTrendingHashtags(headers) {
   return axios.get(`${URL}/hashtags`, headers);
@@ -13,17 +13,37 @@ function getHashtagByName(hashtag, headers) {
 function postPost(body, token) {
   return axios.post(`${URL}/publish`, body, {
     headers: {
-      'Authorization': `Bearer ${token}`
-    }
+      Authorization: `Bearer ${token}`,
+    },
   });
 }
 
 function getPosts(token) {
   return axios.get(`${URL}/timeline`, {
     headers: {
-      'Authorization': `Bearer ${token}`
-    }
+      Authorization: `Bearer ${token}`,
+    },
   });
 }
-
-export { getTrendingHashtags, getHashtagByName, postPost, getPosts };
+function getPageUser(token, id) {
+  return axios.get(`${URL}/user/${id}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+}
+function searchUser(token, name) {
+  return axios.get(`${URL}/timeline/${name}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+}
+export {
+  getTrendingHashtags,
+  getHashtagByName,
+  postPost,
+  getPosts,
+  getPageUser,
+  searchUser,
+};
